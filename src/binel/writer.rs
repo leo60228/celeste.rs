437e25369a38088abc674dd1a3be86e546302364
leo60228 <iakornfeld@gmail.com>
2019-02-25 17:52:40 -0500

    Switch to trait objects instead of templates (0.1.1)

--- src/binel/writer.rs
+++ src/binel/writer.rs
@@ -14,7 +14,7 @@ use super::BinFile;
 ///
 /// assert_eq!(&buf.get_ref()[..], b"\x0bCELESTE MAP");
 /// ```
-pub fn put_string<W: Write>(writer: &mut W, string: &str) -> std::io::Result<()> {
+pub fn put_string(writer: &mut dyn Write, string: &str) -> std::io::Result<()> {
     let mut length_buf = unsigned_varint::encode::usize_buffer();
     let length = unsigned_varint::encode::usize(string.len(), &mut length_buf);
     writer.write_all(length)?;
@@ -25,7 +25,7 @@ pub fn put_string<W: Write>(writer: &mut W, string: &str) -> std::io::Result<()>
 }
 
 /// Write a BinFile. Tested solely in integration tests due to complexity.
-pub fn put_file<W: Write>(mut writer: &mut W, bin: BinFile) -> std::io::Result<()> {
+pub fn put_file(mut writer: &mut dyn Write, bin: BinFile) -> std::io::Result<()> {
     put_string(&mut writer, "CELESTE MAP")?;
 
     put_string(&mut writer, bin.package)?;
