9ebb15d8ca9014349d393d4fcbd8bd834ef853ee
leo60228 <iakornfeld@gmail.com>
2019-02-25 16:25:35 -0500

    add take_string and switch to nom-varint for varints (0.0.3)

--- src/binel/mod.rs
+++ src/binel/mod.rs
@@ -1,2 +1,5 @@
 /// `parser` parses BinaryElement files.
 pub mod parser;
+
+// /// `writer` writers BinaryElement files.
+//pub mod writer;
--- src/binel/parser.rs
+++ src/binel/parser.rs
@@ -1,40 +1,40 @@
+//! (nom doesn't currently support documenting parsers, so I'm putting them in the module
+//! description)
+//!
+//! # `take_string`
+//! Take a string with the length being a varint.
+//!
+//! ## Examples:
+//! ```
+//! use celeste::binel::parser::take_string;
+//!
+//! let header = b"\x0bCELESTE MAP";
+//!
+//! assert_eq!(take_string(&header[..]), Ok((&b""[..], "CELESTE MAP")));
+//! ```
+
 extern crate nom;
 
-use nom::Err::*;
-use nom::Needed::Unknown;
+use std::str;
+use nom_varint::take_varint;
 
-/// Parses a variable-byte length from a map.
-///
-/// # Examples
-///
-/// ```
-/// use celeste::binel::parser::take_length;
-///
-/// assert_eq!(take_length(&[0x0b]), Ok((&[] as &[u8], 0x0b)));
-/// ```
-pub fn take_length(i: &[u8]) -> nom::IResult<&[u8], usize> {
-    let mut res: usize = 0;
-    let mut count: usize = 0;
-    let mut remainder = i;
-    loop {
-        let byte = match take!(remainder, 1) {
-            Ok((rest, bytes)) => {remainder = rest; bytes[0]},
-            Err(_) => return Err(Incomplete(Unknown)),
-        };
-        res += ((byte as usize) & 127) << (count * 7);
-        count += 1;
-        if (byte >> 7) == 0 {
-            return Ok((remainder, res));
-        }
+named!(pub take_string<&[u8], &str>, do_parse!(
+    length: take_varint >>
+    string: take_str!(length) >>
+    ( string )
+));
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn take_header_length() {
+        assert_eq!(take_varint(&b"\x0bCELESTE MAPdummy"[..]), Ok((&b"CELESTE MAPdummy"[..], 0x0b)));
     }
-}
 
-#[test]
-fn parse_length_simple() {
-    assert_eq!(take_length(&[0x0b, 0x01, 0x02, 0x03]), Ok((b"\x01\x02\x03" as &[u8], 11)));
-}
-
-#[test]
-fn parse_length_twobyte() {
-    assert_eq!(take_length(&[0x84, 0x02, 0x04, 0x05, 0x06]), Ok((b"\x04\x05\x06" as &[u8], 260)));
+    #[test]
+    fn take_header() {
+        assert_eq!(take_string(&b"\x0bCELESTE MAPdummy"[..]), Ok((&b"dummy"[..], "CELESTE MAP")));
+    }
 }
