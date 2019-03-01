f3799210e58de3af34c224295d3acc59c21f1954
leo60228 <iakornfeld@gmail.com>
2019-03-01 13:03:23 -0500

    Move celeste_derive tests into celeste

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! # `celeste`
 //! `celeste` is a Rust crate for files used in the 2018 video game Celeste. It currently has a fully-functional writer and parser for `BinaryElement` files, which are used to store the game's levels.
 
+extern crate self as celeste; // necessary for celeste_derive to work
+
 /// `binel` handles Celeste's `BinaryElement` format, which is used for map files.
 pub mod binel;
