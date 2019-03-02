bc5671c9a24e13b78a98fac4b51f5d3f91b7bdff
leo60228 <iakornfeld@gmail.com>
2019-03-02 12:36:49 -0500

    Add error logging to dump_map.rs

--- examples/dump_map.rs
+++ examples/dump_map.rs
@@ -1,4 +1,5 @@
 use celeste::{*, binel::{*, serialize::*}};
+use error_chain::ChainedError;
 
 fn main() {
     env_logger::init();
@@ -6,6 +7,12 @@ fn main() {
     let map_bytes = include_bytes!("empty.bin");
     let map_bin = parser::take_file(map_bytes).unwrap().1;
     println!("{:#?}", map_bin); // pretty print
-    let map_data = maps::Map::from_binel(BinElValue::Element(map_bin.root));
-    println!("{:#?}", map_data); // pretty print
+    let map_data = match maps::Map::from_binel(BinElValue::Element(map_bin.root)) {
+        Ok(map) => map,
+        Err(err) => {
+            println!("{}", err.display_chain().to_string());
+            return;
+        }
+    };
+    println!("{:#?}", map_data);
 }
