06985b00a6199dfdc236b9ba761361c1d5da32c2
leo60228 <iakornfeld@gmail.com>
2019-07-22 18:45:51 -0400

    Experimental nom 5 upgrade

--- src/binel/parser.rs
+++ src/binel/parser.rs
@@ -1,5 +1,11 @@
 use super::*;
 use log::*;
+use nom::branch::alt;
+use nom::bytes::complete::*;
+use nom::combinator::map;
+use nom::multi::count;
+use nom::number::complete::*;
+use nom::sequence::preceded;
 use nom::*;
 use nom_varint::take_varint;
 
@@ -13,33 +19,31 @@ use nom_varint::take_varint;
 ///
 /// assert_eq!(take_string(&header[..]), Ok((&b""[..], "CELESTE MAP".to_string())));
 /// ```
-pub fn take_string(input: &[u8]) -> IResult<&[u8], String> {
-    do_parse!(
-        input,
-        length: take_varint >> string: take_str!(length) >> (string.to_string())
-    )
+pub fn take_string(buf: &[u8]) -> IResult<&[u8], String> {
+    let (buf, length) = take_varint(buf)?;
+    let (buf, string) = take_str!(buf, length)?;
+    Ok((buf, string.to_string()))
 }
 
 /// Lookup a u16 from a `&[u8]` in a string lookup table.
-pub fn take_lookup<'a, 'b>(buf: &'a [u8], lookup: &'b [String]) -> IResult<&'a [u8], &'b String> {
-    let (buf, index) = le_u16(buf)?;
-    Ok((buf, &lookup[index as usize]))
+pub fn take_lookup<'a: 'b, 'b>(
+    lookup: &'b [String]
+) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], &'b String> + 'b {
+    map(le_u16, move |index| &lookup[index as usize])
 }
 
 /// Take a single character from a Celeste RLE-encoded string in a `&[u8]`.
 pub fn take_rle_char(buf: &[u8]) -> IResult<&[u8], String> {
-    do_parse!(
-        buf,
-        times: le_u8 >> byte: le_u8 >> ((byte as char).to_string().repeat(times as usize))
-    )
+    let (buf, times) = le_u8(buf)?;
+    let (buf, byte) = le_u8(buf)?;
+    Ok((buf, (byte as char).to_string().repeat(times as usize)))
 }
 
 /// Take a Celeste RLE-encoded string from a `&[u8]`
 pub fn take_rle_string(buf: &[u8]) -> IResult<&[u8], String> {
-    do_parse!(
-        buf,
-        len: le_i16 >> chars: count!(take_rle_char, (len / 2) as usize) >> (chars.concat())
-    )
+    let (buf, len) = le_i16(buf)?;
+    let (buf, chars) = count(take_rle_char, (len / 2) as usize)(buf)?;
+    Ok((buf, chars.concat()))
 }
 
 /// Parse a `BinElAttr` from a `&[u8]`.
@@ -48,89 +52,102 @@ pub fn take_rle_string(buf: &[u8]) -> IResult<&[u8], String> {
 /// ```
 /// use celeste::binel::*;
 ///
-/// assert_eq!(parser::take_elemattr(b"\x01\x05", &[]), Ok((&b""[..], BinElAttr::Int(5))));
+/// assert_eq!(parser::take_elemattr(&[])(b"\x01\x05"), Ok((&b""[..], BinElAttr::Int(5))));
 /// ```
 #[allow(clippy::unknown_clippy_lints)]
 #[allow(clippy::cognitive_complexity)]
 #[allow(clippy::cyclomatic_complexity)]
-pub fn take_elemattr<'a>(buf: &'a [u8], lookup: &[String]) -> IResult<&'a [u8], BinElAttr> {
-    debug!("taking attribute (type {})", buf[0]);
-    do_parse!(
-        buf,
-        val: alt!(
-            do_parse!(tag!(b"\x00") >> byte: le_u8 >> (BinElAttr::Bool(byte != 0)))
-                | do_parse!(tag!(b"\x01") >> byte: le_u8 >> (BinElAttr::Int(i32::from(byte))))
-                | do_parse!(tag!(b"\x02") >> word: le_i16 >> (BinElAttr::Int(i32::from(word))))
-                | do_parse!(tag!(b"\x03") >> dword: le_i32 >> (BinElAttr::Int(dword)))
-                | do_parse!(tag!(b"\x04") >> float: le_f32 >> (BinElAttr::Float(float)))
-                | do_parse!(
-                    tag!(b"\x05")
-                        >> string: apply!(take_lookup, lookup)
-                        >> (BinElAttr::Text(string.to_string()))
-                )
-                | do_parse!(
-                    tag!(b"\x06") >> string: take_string >> (BinElAttr::Text(string.to_string()))
-                )
-                | do_parse!(
-                    tag!(b"\x07")
-                        >> string: take_rle_string
-                        >> (BinElAttr::Text(string.to_string()))
-                )
-        ) >> ({
-            debug!("{:?}", val);
-            val
-        })
-    )
+pub fn take_elemattr<'a: 'b, 'b>(
+    lookup: &'b [String]
+) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], BinElAttr> + 'b {
+    alt((
+        preceded(
+            tag(b"\x00"),
+            map(le_u8, |byte: u8| BinElAttr::Bool(byte != 0))
+        ),
+        preceded(
+            tag(b"\x01"),
+            map(le_u8, |byte: u8| BinElAttr::Int(i32::from(byte)))
+        ),
+        preceded(
+            tag(b"\x02"),
+            map(le_i16, |word: i16| BinElAttr::Int(i32::from(word)))
+        ),
+        preceded(
+            tag(b"\x03"),
+            map(le_i32, |dword: i32| BinElAttr::Int(i32::from(dword)))
+        ),
+        preceded(
+            tag(b"\x04"),
+            map(le_f32, |float: f32| BinElAttr::Float(float))
+        ),
+        preceded(
+            tag(b"\x05"),
+            map(take_lookup(lookup), |string: &String| {
+                BinElAttr::Text(string.clone())
+            })
+        ),
+        preceded(
+            tag(b"\x06"),
+            map(take_string, |string: String| BinElAttr::Text(string))
+        ),
+        preceded(
+            tag(b"\x07"),
+            map(take_rle_string, |string: String| BinElAttr::Text(string))
+        )
+    ))
 }
 
 /// Parse a `BinEl` from a `&[u8]`. Tested solely in integration tests due to complexity.
-pub fn take_element<'a>(buf: &'a [u8], lookup: &[String]) -> IResult<&'a [u8], BinEl> {
-    debug!("taking element");
-    let mut buf = buf;
+pub fn take_element<'a: 'b, 'b>(
+    lookup: &'b [String]
+) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], BinEl> + 'b {
+    move |buf| {
+        debug!("taking element");
+        let mut buf = buf;
 
-    debug!("taking name");
-    let (lookup_buf, name) = take_lookup(buf, &lookup)?;
-    buf = lookup_buf;
-    debug!("name is {}", name);
+        debug!("taking name");
+        let (lookup_buf, name) = take_lookup(&lookup)(buf)?;
+        buf = lookup_buf;
+        debug!("name is {}", name);
 
-    let mut binel = BinEl::new(&name);
+        let mut binel = BinEl::new(&name);
 
-    let (attr_count_buf, attr_count) = le_u8(buf)?;
-    buf = attr_count_buf;
+        let (attr_count_buf, attr_count) = le_u8(buf)?;
+        buf = attr_count_buf;
 
-    for _ in 0..attr_count {
-        debug!("taking key");
-        let (key_buf, key) = take_lookup(buf, &lookup)?;
-        debug!("key is {}", key);
-        debug!("taking value");
-        let (val_buf, val) = take_elemattr(key_buf, &lookup)?;
-        buf = val_buf;
-        binel.attributes.insert(key.clone(), val);
+        for _ in 0..attr_count {
+            debug!("taking key");
+            let (key_buf, key) = take_lookup(&lookup)(buf)?;
+            debug!("key is {}", key);
+            debug!("taking value");
+            let (val_buf, val) = take_elemattr(&lookup)(key_buf)?;
+            buf = val_buf;
+            binel.attributes.insert(key.clone(), val);
+        }
+
+        let (mut buf, child_count) = le_u16(buf)?;
+        debug!("{} children", child_count);
+        for _ in 0..child_count {
+            let (child_buf, child) = take_element(lookup)(buf)?;
+            buf = child_buf;
+            binel.insert(child);
+        }
+
+        debug!("got element");
+
+        Ok((buf, binel))
     }
-
-    let (mut buf, child_count) = le_u16(buf)?;
-    debug!("{} children", child_count);
-    for _ in 0..child_count {
-        let (child_buf, child) = take_element(buf, &lookup)?;
-        buf = child_buf;
-        binel.insert(child);
-    }
-
-    debug!("got element");
-
-    Ok((buf, binel))
 }
 
 /// Parse a `BinFile` from a `&[u8]`. Tested solely in integration tests due to complexity.
-pub fn take_file(input: &[u8]) -> IResult<&[u8], BinFile> {
-    do_parse!(
-        input,
-        tag!(b"\x0bCELESTE MAP")
-            >> package: take_string
-            >> lookup: length_count!(le_i16, take_string)
-            >> root: apply!(take_element, &lookup)
-            >> (BinFile { package, root })
-    )
+pub fn take_file(buf: &[u8]) -> IResult<&[u8], BinFile> {
+    let (buf, _) = tag(b"\x0bCELESTE MAP")(buf)?;
+    let (buf, package) = take_string(buf)?;
+    let (buf, length) = le_i16(buf)?;
+    let (buf, lookup) = count(take_string, length as usize)(buf)?;
+    let (buf, root) = take_element(&lookup)(buf)?;
+    Ok((buf, BinFile { package, root }))
 }
 
 #[cfg(test)]
