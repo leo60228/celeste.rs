37d1944b7153de9a6d3e86f20eb6f3d3f4c2fdc2
leo60228 <iakornfeld@gmail.com>
2020-04-09 11:23:24 -0400

    fmt

--- celeste/src/dialog/parser.rs
+++ celeste/src/dialog/parser.rs
@@ -3,8 +3,8 @@ use std::prelude::v1::*;
 use super::{Dialog, DialogEntry, DialogKey};
 use crate::Result;
 
-use pest_derive::*;
 use pest::Parser;
+use pest_derive::*;
 
 #[derive(Parser)]
 #[grammar = "dialog/dialog.pest"]
@@ -12,16 +12,33 @@ struct DialogParser;
 
 pub fn parse(input: &str) -> Result<Dialog> {
     let mut map = Dialog::new();
-    let file = DialogParser::parse(Rule::file, &input).unwrap_or_else(|e| panic!("{}", e)).next().expect("file always produces output upon a successful parse");
+    let file = DialogParser::parse(Rule::file, &input)
+        .unwrap_or_else(|e| panic!("{}", e))
+        .next()
+        .expect("file always produces output upon a successful parse");
     for rule in file.into_inner() {
         match rule.as_rule() {
             Rule::comment => {}
             Rule::entry => {
                 let mut inner = rule.into_inner();
-                let level = inner.next().expect("entry always has 3 inner rules").as_str().len() + 1;
-                let key = inner.next().expect("entry always has 3 inner rules").as_str();
-                let indented_str = inner.next().expect("entry always has 3 inner rules").as_str();
-                let entry = DialogEntry { level, indented_str };
+                let level = inner
+                    .next()
+                    .expect("entry always has 3 inner rules")
+                    .as_str()
+                    .len()
+                    + 1;
+                let key = inner
+                    .next()
+                    .expect("entry always has 3 inner rules")
+                    .as_str();
+                let indented_str = inner
+                    .next()
+                    .expect("entry always has 3 inner rules")
+                    .as_str();
+                let entry = DialogEntry {
+                    level,
+                    indented_str,
+                };
                 map.insert(DialogKey(key, entry));
             }
             _ => unreachable!(),
